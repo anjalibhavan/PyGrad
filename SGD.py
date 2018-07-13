9a1a2f8eca0ce0eb4d3f20006ad365bcda6ff57b
Anjali Bhavan <anjaliblogger@gmail.com>
2018-07-14 01:02:57 +0530

    Stochastic Gradient Descent

--- SGD.py
+++ SGD.py
@@ -8,10 +8,10 @@ import numpy as np
 
 # Load dataset and split into train and test sets
 
-diabetes=load_diabetes()
+diabetes = load_diabetes()
 Xdata = diabetes.data
 ydata = diabetes.target
-x_train,x_test,y_train,y_test=train_test_split(Xdata,ydata,random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(Xdata,ydata,random_state=42)
 
 # To add a dataset of your own, uncomment the following lines and comment out the previous four lines:
 '''
@@ -24,26 +24,26 @@ x_train,x_test,y_train,y_test = train_test_split(Xdata,ydata,random_state=42)
 
 # Initialize training parameters
 
-n_iterations=100
-thetaa=np.zeros(x_train.shape[1])
-alpha=0.5
+n_iterations = 100
+thetaa = np.zeros(x_train.shape[1])
+alpha = 0.5
 
 # Stochastic Gradient Descent
 
 def SGD(X,theta,y):
-    X_trans=X.transpose()
-    gradient=0
+    X_trans = X.transpose()
+    gradient = 0
     for i in range(1,n_iterations):
-        X_new=shuffle(X)
+        X_new = shuffle(X)
         for q in range(0,X.shape[0]):
-            p=np.random.randint(0,X_new.shape[0])
-            h=np.dot(X_new[p],theta)
-            loss=h-y[p]
-            gradient=np.dot(X_trans[:,p],loss)
-            theta=theta-alpha*gradient
+            p = np.random.randint(0,X_new.shape[0])
+            h = np.dot(X_new[p],theta)
+            loss = h-y[p]
+            gradient = np.dot(X_trans[:,p],loss)
+            theta = theta-alpha*gradient
     return theta
 
 # Calculate new weights and print them
 
-new_theta=SGD(x_train,thetaa,y_train)
+new_theta = SGD(x_train,thetaa,y_train)
 print(new_theta)
