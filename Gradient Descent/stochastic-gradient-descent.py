0b1b98ef909f96c53dfb25fda11644d191955f61
Anjali Bhavan <anjaliblogger@gmail.com>
2018-07-19 21:03:35 +0530

    Update stochastic-gradient-descent.py

--- Gradient Descent/stochastic-gradient-descent.py
+++ Gradient Descent/stochastic-gradient-descent.py
@@ -1,49 +1,16 @@
 
-from sklearn.datasets import load_diabetes
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.utils import shuffle
-import numpy as np
-
-# Load dataset and split into train and test sets
-
-diabetes = load_diabetes()
-Xdata = diabetes.data
-ydata = diabetes.target
-x_train, x_test, y_train, y_test = train_test_split(Xdata,ydata,random_state=42)
-
-# To add a dataset of your own, uncomment the following lines and comment out the previous four lines:
-'''
-import pandas as pd
-cancer=pd.read_csv('path of csv file')
-Xdata=cancer.iloc[:,:] #set the indices as per the column orientation in the csv file
-ydata=cancer.iloc[:,:] #set the indices as per the column orientation in the csv file
-x_train,x_test,y_train,y_test = train_test_split(Xdata,ydata,random_state=42)
-'''
-
-# Initialize training parameters
-
-n_iterations = 100
-thetaa = np.zeros(x_train.shape[1])
-alpha = 0.5
-
 # Stochastic Gradient Descent
 
-def SGD(X,theta,y):
+def StochasticGradientDescent(X,y,theta,n_epochs,learning_rate):
     X_trans = X.transpose()
     gradient = 0
-    for i in range(1,n_iterations):
+    for i in range(1,n_epochs):
         X_new = shuffle(X)
         for q in range(0,X.shape[0]):
             p = np.random.randint(0,X_new.shape[0])
-            h = np.dot(X_new[p],theta)
-            loss = h-y[p]
+            hypothesis = np.dot(X_new[p],theta)
+            loss = hypothesis-y[p]
             gradient = np.dot(X_trans[:,p],loss)
-            theta = theta-alpha*gradient
+            theta = theta-learning_rate * gradient
     return theta
 
-# Calculate new weights and print them
-
-new_theta = SGD(x_train,thetaa,y_train)
-print(new_theta)
