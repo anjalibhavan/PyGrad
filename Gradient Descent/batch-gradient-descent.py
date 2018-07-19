8b21111236530fcd896587fa157f27982154bb2a
Anjali Bhavan <anjaliblogger@gmail.com>
2018-07-19 21:04:47 +0530

    Update

--- Gradient Descent/batch-gradient-descent.py
+++ Gradient Descent/batch-gradient-descent.py
@@ -1,4 +1,4 @@
-def batch_gradient_descent(X,y,theta=np.empty(),learning_rate=0.1,num_epochs=100):
+def BatchGradientDescent(X,y,theta,learning_rate=0.1,num_epochs=100):
     X_trans=X.transpose()
     gradient=0
     for i in range(0,num_epochs):
